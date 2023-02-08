01d6a9029f30ba182c3bd2b44243cc3ca2a70115
jeff <jeffmwazala@gmail.com>
2023-02-08 19:20:35 +0300

    I have created my first App using Compose a

---
+++ app/src/main/java/com/hope/blessariahsapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.hope.blessariahsapp
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.hope.blessariahsapp.ui.theme.BlessariahsAppTheme
+import kotlin.String as String1
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlessariahsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                   Jeff(message = "This is the day Enjoy", from ="from Jeff" )
+                    }
+
+                  }
+                }
+            }
+        }
+@Composable
+fun Jeff(message:kotlin.String ,from:kotlin.String) {
+    val image = painterResource(id= R.drawable.rhino)
+
+Box() {
+    Image(painter = image, contentDescription = null)
+    Column() {
+        Text(text = message, fontSize = 20.sp, color = Color.Red)
+        Text(text = from, fontSize = 15.sp, color = Color.Black)
+    }
+}
+
+}
+@Preview
+@Composable
+fun Jeffpreview()
+{
+    Jeff(message="This is the day Enjoy", from= " from Jeff" )
+}
+
+
+
